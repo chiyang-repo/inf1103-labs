325c98eb885e13a4ea98321053dc1739ab39dc0a
Chi Yang <2603046@sit.singaporetech.edu.sg>
2026-09-18 10:31:41 +0800

    comments added

--- Lab 3/modular_auditor.py
+++ Lab 3/modular_auditor.py
@@ -1,4 +1,5 @@
 
+# Validate user input function
 def get_valid_input(user_input):
     input = user_input
     try:
@@ -6,46 +7,49 @@ def get_valid_input(user_input):
             print("\nInvalid input. Please enter a non-negative number.")
             return None
         else:
-            return int(input)
-    except ValueError:
+            return int(input) # Valid input, return the integer value
+    except ValueError: #catch the ValueError if user input is not a number
         if input.lower() == 'quit': #exit the program if user types 'quit'
             return 'quit' #Exit the loop and program
-        else: # Increment failed entry
+        else: # Increment failed entry for any other string input
             print("\nInvalid input. Please enter a valid number or type 'quit' to exit.")
             return None
         
 
 
-
+# Process delivery function
 def process_delivery(quantity, cost_per_unit):
     return quantity * cost_per_unit
 
+# Calculate tax function
 def calculate_tax(delivery_cost):
     tax_rate = 0.10
     return delivery_cost * tax_rate
 
+# Generate report function
 def generate_report(inventory, delivery_cost, count):
     print("\n--- Delivery Report ---")
     print(f"Total Deliveries Processed: {inventory}")
     print(f"Total Delivery Cost: {delivery_cost}")
     print(f"Number of Failed/Rejected Entries: {count}")
 
-
+# Main function
 def main():
-    # Inventory stocks
+    # Inventory stocks upon initialization
     inventory = 0
     count = 0
     cost_per_unit = 0.5
+
     while True:
         print(f"\nCurrent inventory: {inventory}") # Display current inventory
         user_input = input("\nEnter the number of items to add/update to inventory (or type 'quit' to quit): ") # Get user input
-        validated_user_input = get_valid_input(user_input)
+        validated_user_input = get_valid_input(user_input) #run the user input through the validation function
         if validated_user_input != 'quit' and validated_user_input is not None: # Check if user input is not 'quit' and not a failed entry
             if validated_user_input > 0 : # Check if user input is not zero
                 inventory += validated_user_input # Increment inventory by user input
                 print(f"\nInventory updated. Current stock: {inventory}")
             else:
-                print("\n No changes made.")
+                print("\nNo changes made.") # User input is zero, no changes made to inventory
         elif validated_user_input is None:
             count += 1 # Increment failed entry
         else:
